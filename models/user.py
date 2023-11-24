c6491226048784696db546d263ef9a235b927aa1
DexyMaroni <divineinyang7@gmail.com>
2023-11-24 09:38:37 -0800

    Task &

--- models/user.py
+++ models/user.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """This class deals with the personal details of a user"""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable =False)
+    password = Column(String(128), nullable =False)
+    first_name = Column(String(123))
+    last_name = Column((123)
+
