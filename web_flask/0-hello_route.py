d202f2a9cc9d6843764b7276c6bebe81f70ce16f
DexyMaroni <divineinyang7@gmail.com>
2024-01-03 22:05:13 +0000

    Web app_1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Initializing a flask web app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def greeting():
+    """A function that prints a welcome message when initiated"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run
