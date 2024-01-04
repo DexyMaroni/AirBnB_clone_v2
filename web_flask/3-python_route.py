356d990e70c153a17d8004e281fc1fdceeb0dde4
DexyMaroni <divineinyang7@gmail.com>
2024-01-04 11:59:03 +0000

    # Replace underscores with spaces in the text variable

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" A script thatstarts a web application and has two routes. """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def greeting():
+    """A function that prints a welcome message when initiated"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    """ A function that prints HBNB """
+    return "HBNB"
+
+@app.route("/c/<text>")
+def text(text):
+    """ Replace underscores with spaces in the text variable """
+     return 'C' + text.replace('_', ' ')
+
+@app.route("/python/<text>")
+def python_text(text = "is cool"):
+    return "Python" + text.replace('_', ' ')
+
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host= '0.0.0.0', port = 5000)
