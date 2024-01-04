aaa71e7b491cd4f4a58c685c33d75a388277fd97
DexyMaroni <divineinyang7@gmail.com>
2024-01-04 16:37:58 +0000

    Web App

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -35,6 +35,12 @@ def python_with_text(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
+@app.route('/number/<int:n>')
+def number(n=None):
+    """Allow request if path variable is a valid integer
+    """
+    return str(n) + ' is a number'
+
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
