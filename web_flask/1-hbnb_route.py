a0a674586993a0c28e3f32dc554682f4437d318e
DexyMaroni <divineinyang7@gmail.com>
2024-01-04 12:24:42 +0000

    checks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-
-""" A script thatstarts a web application and has two routes. """
+"""Start web application with two routings
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/")
-def greeting():
-    """A function that prints a welcome message when initiated"""
-    return "Hello HBNB!"
+@app.route('/')
+def hello():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
 
-@app.route("/hbnb")
+
+@app.route('/hbnb')
 def hbnb():
-    """ A function that prints HBNB """
-    return "HBNB"
+    """Return string when route queried
+    """
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
-    app.run(host= '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
